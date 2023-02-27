8e577db1a9ecbeb234e7a7cf537c1ed247868c86
Rodrigo <contactorodrigocabrera@gmail.com>
2023-02-27 11:49:18 -0300

    correcciones en schema de Users.js y en router.post de routes/users.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,14 @@ import mongoose from "mongoose";
 let schema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, requiered: true},
-    password: {type: String, requiered: true}
+    password: {type: String, requiered: true},
+    photo: { type: String },
+    is_online: { type: Boolean },
+    is_admin: { type: Boolean },
+    is_author: { type: Boolean },
+    is_company: { type: Boolean },
+    is_verified: { type: Boolean, required: true },
+    verify_code: { type: String, required: true }
 },{
     timestamps: true
 })
--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,18 @@ router.get('/', (req, res, next) => {
 router.post('/', 
   async (req, res) =>{
     try {
+        req.body.photo = false;
+        req.body.is_online = false;
+        req.body.is_admin = false;
+        req.body.is_author = false;
+        req.body.is_company = false;
+        req.body.is_verified = false;
+        req.body.verify_code = false;
         let user = await User.create(req.body)
-        return res.status(201).send("Usuario creado correctamente.")
+        return res.status(201).json({
+          success: true, 
+          message: "Usuario creado correctamente."
+        })
     } catch(error){
       console.log(error)
       return res.status(400).send("No se pudo crear el usuario.")
