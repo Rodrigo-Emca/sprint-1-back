1cb056ef05725846bd1c2f16eaed308e429a1f8c
Alexis Franco <ronaldofranco196@gmail.com>
2023-03-06 11:38:53 -0300

    correcion middleware

--- middleware/chapters/exists_order.js
+++ middleware/chapters/exists_order.js
@@ -1,14 +1,19 @@
 import Chapter from '../../models/Chapter.js'
 
 async function existsOrder(req, res, next){
-    const chapter = await Chapter.findOne({order: req.body.order})
+    req.body.manga_id= '63ffafade652fa554fe009eb'
+    console.log(req.body)
+    const chapter = await Chapter.findOne({manga_id: req.body.manga_id, order: req.body.order})
+    console.log(chapter)
+    if(!chapter){
+        return next()
+    }
     if(chapter){
         return res.status(400).json({
             success: false,
             message: 'el order ya existe'
         })
     }
-    next()
 }
 
 export default existsOrder
