ea76aed5cec51322159ca7b3ad60b010481875ec
greta <greta.pereda@gmail.com>
2023-03-09 12:47:11 -0600

    controlador y ruta de busqueda de mangas por titulo y por categoria

--- controllers/manga.controller.js
+++ controllers/manga.controller.js
@@ -18,8 +18,48 @@ const controller = {
         message: "no se pudo crear un manga",
       });
     }
-  } 
+  },
+  get_mangas: async (req, res, next) => {
+    let consultas = {}
+
+    let pagination = {
+      page: 1, 
+      limit: 6 
+    }
+
+    if (req.query.title) {
+      consultas.title = new RegExp(req.query.title.trim(),'i')
+
+    }
+    if (req.query.category) {
+      consultas.category = new RegExp(req.query.category.trim(),'i')
+
+    }
+    if (req.query.page) {
+      pagination.page = req.query.page
+    }
+    if (req.query.quantity) { 
+      pagination.limit = req.query.quantity
+    }
+    try {
+      let all = await Manga.find(consultas)
+      .select('title category -_id')
+      .sort({ title: 1, /* category: -1 */ })
+      
+      .skip( pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0 )
+      .limit( pagination.limit > 0 ? pagination.limit : 0 )
+
+      return res
+      .status(200)
+      .json({ mangas: all})
+    }
+    catch(err) {
+      next(err)
+    }
+  }
 
 }
 
 export default controller
+
+//http://localhost:8080/mangas/read?title= m&category= ma
--- routes/mangas.js
+++ routes/mangas.js
@@ -4,18 +4,17 @@ import schema from '../schemas/manga.js'
 import validator from '../middlewares/validator.js'
 import validator_title from '../middlewares/mangas/exists_title.js'
 //import is_active from '../middlewares/authors/is_active.js' //MIDDLEWARE M06
-
-
 import create_manga from '../controllers/manga.controller.js'
 
 
 let router = express.Router();
 const { show } = mostrar_categoriescontroller
 const { create } = create_manga
-
+const { get_mangas } = create_manga
 
 router.get("/", show)
 //router.post("/", validator(mangaCreate),validator_title,is_active,create) //RUTA CON VALIDADOR DEL M06
 router.post("/", validator(schema),validator_title,create)
+router.get('/read', get_mangas)
 
 export default router;
