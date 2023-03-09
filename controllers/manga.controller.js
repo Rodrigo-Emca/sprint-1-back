427ae1eac8997d1f85d748befb8917e4e8fba6fb
Rodrigo <contactorodrigocabrera@gmail.com>
2023-03-09 16:00:33 -0300

    controlador get_one creado. Endpoint agregado al enrutador de mangas. Funciona con petición de postman

--- controllers/manga.controller.js
+++ controllers/manga.controller.js
@@ -18,8 +18,66 @@ const controller = {
         message: "no se pudo crear un manga",
       });
     }
-  } 
+  },
 
+  get_mangas: async (req, res, next) => {
+    let consultas = {}
+
+    let pagination = {
+      page: 1, 
+      limit: 6 
+    }
+
+    if (req.query.title) {
+      consultas.title = new RegExp(req.query.title.trim(),'i')
+
+    }
+    if (req.query.category) {
+      consultas.category = new RegExp(req.query.category.trim(),'i')
+
+    }
+    if (req.query.page) {
+      pagination.page = req.query.page
+    }
+    if (req.query.quantity) { 
+      pagination.limit = req.query.quantity
+    }
+    try {
+      let all = await Manga.find(consultas)
+      .select('title category -_id')
+      .sort({ title: 1, /* category: -1 */ })
+
+      .skip( pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0 )
+      .limit( pagination.limit > 0 ? pagination.limit : 0 )
+
+
+      return res
+      .status(200)
+      .json({ mangas: all})
+    }
+    catch(err) {
+      next(err)
+    }
+  },
+
+  get_one: async (req, res, next) => {
+    let query = {}
+    if (req.query._id) { query._id = req.query._id }
+    try {
+      let one = await Manga.findById(query)
+        // .select("name -_id")
+      return res
+        .status(200)
+        .json({ 
+          mangas: {
+            title: one.title, 
+            decription: one.description, 
+            category: one.category}})
+    }
+    catch(err) {
+      next(err)
+    }
+  }
 }
 
 export default controller
--- routes/mangas.js
+++ routes/mangas.js
@@ -4,18 +4,24 @@ import schema from '../schemas/manga.js'
 import validator from '../middlewares/validator.js'
 import validator_title from '../middlewares/mangas/exists_title.js'
 //import is_active from '../middlewares/authors/is_active.js' //MIDDLEWARE M06
-
-
 import create_manga from '../controllers/manga.controller.js'
 
 
 let router = express.Router();
 const { show } = mostrar_categoriescontroller
 const { create } = create_manga
-
+const { get_mangas } = create_manga
+const { get_one } = create_manga
 
 router.get("/", show)
 //router.post("/", validator(mangaCreate),validator_title,is_active,create) //RUTA CON VALIDADOR DEL M06
 router.post("/", validator(schema),validator_title,create)
+router.get('/read', get_mangas)
+router.get('/:id', get_one)
 
 export default router;
+
+
+//Para postman: 
+//http://localhost:8000/mangas/read?title= manga&category=d
+//http://localhost:8000/mangas/:id
