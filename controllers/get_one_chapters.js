6d47e4656842d09ef978424705c4035be06a698a
Alexis Franco <ronaldofranco196@gmail.com>
2023-03-10 18:49:39 -0300

    corrección chapter one

--- controllers/get_one_chapters.js
+++ controllers/get_one_chapters.js
@@ -3,11 +3,11 @@ import Chapter from "../models/Chapter.js"
 const controllerChapter = {
     chapter: async (req, res) =>{
         try{
-            let chapters = await Chapter.findOne({_id: req.params.id}).select('title order pages -_id')
-            if(chapters){
+            let chapter = await Chapter.findOne({_id: req.params.id}).select('title order pages -_id')
+            if(chapter){
                 return (
                     res.status(200).json({
-                        Chapters: chapters
+                        Chapters: chapter
                     })
                 )
             }
