294405415dad3a26826caa5334987488fb0cae88
Alexis Franco <ronaldofranco196@gmail.com>
2023-03-10 18:48:52 -0300

    corrección get one

--- controllers/get_one_chapters.js
+++ controllers/get_one_chapters.js
@@ -3,19 +3,20 @@ import Chapter from "../models/Chapter.js"
 const controllerChapter = {
     chapter: async (req, res) =>{
         try{
-            if(Chapter){
-                let allChapters = await Chapter.findOne({_id: req.params.id}).select('title order pages -_id')
+            let chapters = await Chapter.findOne({_id: req.params.id}).select('title order pages -_id')
+            if(chapters){
                 return (
-                    res.status(201).json({
-                        Chapters: allChapters
+                    res.status(200).json({
+                        Chapters: chapters
                     })
                 )
             }
         }
         catch(error){
             res.status(404).json({
-                message:'The chapter does not exist'
+                message:'The chapter does not exist',
             })
+            next(error)
         }
     }
 }
