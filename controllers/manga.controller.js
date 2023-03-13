1a1be6724e33f091cff6bbb9ab66452c92fa46a5
greta <greta.pereda@gmail.com>
2023-03-13 15:14:16 -0600

    cambios controlador manga, consultas query por title y por categoria funcionando

--- controllers/manga.controller.js
+++ controllers/manga.controller.js
@@ -34,14 +34,11 @@ const controller = {
       pagination.limit = 10
     }
 
-/*     if (req.query.category) { // ASI ESTABA EL JUEVES! fncionaba
-      consultas.category = new RegExp(req.query.category.trim(),'i')
-    } */
-
     if (req.query.category_id) {
-      //  consultas.category_id = new RegExp(req.query.category_id.trim(),'i')
         //consultas['category_id.name'] = req.query.category_id
-        consultas.category_id = req.query.category_id
+        const categ = req.query.category_id.split(',');//con split lo captura por comas, y se transforma en un array
+        consultas.category_id = {$in:categ}// ahora valida si existe, crea un objeto, si existe en la consulta la devuelve
+        pagination.limit = 10
     } 
  
     if (req.query.page) {
@@ -56,16 +53,8 @@ const controller = {
       .sort({ title: 1})  
       .skip( pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0 )//.skip--corta desde donde
       .limit( pagination.limit > 0 ? pagination.limit : 0 )//hasta donde
-     /*  .populate("category_id", "name -_id") */
-      .populate({
-        path: 'category_id',
-        select: 'name',
-        /* populate: {
-          path: '',
-          select: ''
-        }  */
-      })
-    
+      .populate("category_id", "name -_id")//accede al name de la categoria
+
       return res.status(200).json({ 
         success: true,
         message: "All mangas",
@@ -82,64 +71,7 @@ const controller = {
 }
 
 export default controller
-//http://localhost:8000/mangas/read?title= n&category= s
-//http://localhost:8000/mangas/read?title= t&category=6403f3055c8203c8d0eace85
 
-//http://localhost:8000/mangas/read?category_id=640a29d5aa914d6b7c5846e9
 //http://localhost:8000/mangas/read?title= a &category_id=640a29d5aa914d6b7c5846e9 // ok anda
+// http://localhost:8000/mangas/read?title=a&category_id=640a29d5aa914d6b7c5846e9,640a29d5aa914d6b7c5846ec //ok anda
 
-
-/* 
-  if (req.query.category) {
-    let categ = req.query.category.split(',')
-    console.log(categ)
-    consultas.category_id = req.query.category
-  } 
-
-*/
-
-
-
-
-/* 
-all: async (req, res, next) => {
-    let consultas = {}
-
-    let pagination = {
-      page: 1, 
-      limit: 6 
-    }
-
-    if (req.query.title) {
-      consultas.title = new RegExp(req.query.title.trim(),'i')
-
-    }
-    if (req.query.category) {
-      consultas.category = new RegExp(req.query.category.trim(),'i')
-
-    }
-    if (req.query.page) {
-      pagination.page = req.query.page
-    }
-    if (req.query.quantity) { 
-      pagination.limit = req.query.quantity
-    }
-    try {
-      let all = await Manga.find(consultas)
-      .select('title category -_id')
-      .sort({ title: 1, /* category: -1 / })
-
-      .skip( pagination.page > 0 ? (pagination.page-1)pagination.limit : 0 )
-      .limit( pagination.limit > 0 ? pagination.limit : 0 )
-
-
-      return res
-      .status(200)
-      .json({ mangas: all})
-    }
-    catch(err) {
-      next(err)
-    }
-  }
-
-*/
