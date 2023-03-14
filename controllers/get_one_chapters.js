605485b544b95ae911e1476322a1bf27f4e1f7e7
Alexis Franco <ronaldofranco196@gmail.com>
2023-03-14 16:55:25 -0300

    correcion controlador

--- controllers/get_one_chapters.js
+++ controllers/get_one_chapters.js
@@ -2,17 +2,28 @@ import Chapter from "../models/Chapter.js"
 
 const controllerChapter = {
     chapter: async (req, res) =>{
-        try{
-            let chapter = await Chapter.findOne({_id: req.params.id}).select('title order pages -_id')
-            if(chapter){
-                return (
-                    res.status(200).json({
-                        Chapters: chapter
-                    })
-                )
+        try {
+            let chapter = await Chapter.findById(req.params.id)
+                .select('-__v -updatedAt -createdAt -_id')
+
+            let next = await Chapter.findOne({manga_id: chapter.manga_id, order: chapter.order + 1 })
+            let prev = await Chapter.findOne({manga_id: chapter.manga_id, order: chapter.order - 1 })
+            
+            if (chapter) {
+
+                return res.status(200).json({
+                    success: true,
+                    chapter,
+                    prev: prev?._id,
+                    next: next?._id
+                })
             }
-        }
-        catch(error){
+
+            return res.status(404).json({
+                success: false,
+            })
+
+        } catch (error) {
             next(error)
         }
     }
